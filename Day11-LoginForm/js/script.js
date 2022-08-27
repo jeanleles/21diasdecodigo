801309524d1951d7bd0c41e27b5ecd39ad754ef8
Jean Leles <jeanleles.sa@gmail.com>
2022-08-27 10:56:15 -0300

    Correcao na logica de ativar o botao login ao preencher o campo e-mail

--- Day11-LoginForm/js/script.js
+++ Day11-LoginForm/js/script.js
@@ -2,15 +2,16 @@ const username = document.getElementById('email')
 const password = document.getElementById('password')
 
 username.addEventListener('input', () => {
-    if(username.value.length != '' & password.value.length > 5) {
-        document.getElementsByClassName('button-login').classList.add('button-enabled')
+    if(username.value.length >= 3 & password.value.length > 5) {
+      console.log(username.value.length)
+        document.getElementById('login').classList.add('button-enabled')
     } else {
         document.getElementById('login').classList.remove('button-enabled') 
     }
 })
 
 password.addEventListener('input', () => {
-    if(username.value.length != '' & password.value.length > 5) {
+    if(username.value.length >= 3 & password.value.length > 5) {
         document.getElementById('login').classList.add('button-enabled')
     } else {
         document.getElementById('login').classList.remove('button-enabled') 

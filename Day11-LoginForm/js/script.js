72b0fa062f02d36cb48611f128f1bc8891453c13
Jean Leles <jeanleles.sa@gmail.com>
2022-08-27 00:21:38 -0300

    Subindo Day11-LoginForm

---
+++ Day11-LoginForm/js/script.js
@@ -0,0 +1,19 @@
+const username = document.getElementById('email')
+const password = document.getElementById('password')
+
+username.addEventListener('input', () => {
+    if(username.value.length != '' & password.value.length > 5) {
+        document.getElementsByClassName('button-login').classList.add('button-enabled')
+    } else {
+        document.getElementById('login').classList.remove('button-enabled') 
+    }
+})
+
+password.addEventListener('input', () => {
+    if(username.value.length != '' & password.value.length > 5) {
+        document.getElementById('login').classList.add('button-enabled')
+    } else {
+        document.getElementById('login').classList.remove('button-enabled') 
+    }
+})
+//@jeanleles

be8ff6a8ae87c88820f8a03184bad7cd3c01102d
Jean Leles <jeanleles.sa@gmail.com>
2022-08-28 14:23:19 -0300

    Day12-GaleriaDeImagens

--- Day11-LoginForm/js/script.js
+++ Day11-LoginForm/js/script.js
@@ -3,7 +3,6 @@ const password = document.getElementById('password')
 
 username.addEventListener('input', () => {
     if(username.value.length >= 3 & password.value.length > 5) {
-      console.log(username.value.length)
         document.getElementById('login').classList.add('button-enabled')
     } else {
         document.getElementById('login').classList.remove('button-enabled') 

f4eb58b76bd9d49c02f99ef61bea8b1c5523d08e
Jean Leles <jeanleles.sa@gmail.com>
2022-09-03 23:09:59 -0300

    Subindo Day19 - Galeria de imagens com a API Unsplash

---
+++ Day19-UnsplashAPI/js/script.js
@@ -0,0 +1,34 @@
+const checkbox = document.querySelector('.checkbox')
+
+checkbox.addEventListener('change', () => {
+  document.body.classList.toggle('dark-mode1');
+  document.querySelector('.top').classList.toggle('dark-mode2');
+  document.querySelector('.bg-button').classList.toggle('bg-button-dark');
+  document.querySelector('.more-btn').classList.toggle('more-btn-dark');
+})
+
+const gallery = document.querySelector('.gallery')
+
+function getPhotos() {
+  let photoList = ''
+  fetch('https://api.unsplash.com/photos/?client_id=???')
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (photos) {
+      let index = 1
+      photos.forEach(item => {
+        photoList += `
+          <img class="img-item" src="${item.urls.small}" alt="">
+        `
+        index++
+      })
+      gallery.innerHTML = photoList
+    })
+}
+
+const btnload = document.querySelector('.btn-load')
+
+btnload.addEventListener('click', () => {
+  getPhotos()
+})
